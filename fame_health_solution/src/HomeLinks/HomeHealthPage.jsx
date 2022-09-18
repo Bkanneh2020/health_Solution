99cc28443fd5caaf0e673ed004cb4a5124594af8
Boa Kanneh <bk2162003@yahoo.com>
2022-09-18 15:17:00 -0500

    9/18/2022 second chamges

--- fame_health_solution/src/HomeLinks/HomeHealthPage.jsx
+++ fame_health_solution/src/HomeLinks/HomeHealthPage.jsx
@@ -1,40 +1,10 @@
 import React from "react";
 
 const House = () => {
-  const Healthlinks = [
-    {
-      title: "Nursing",
-    },
-    {
-      title: "Therapy in Session",
-    },
-    {
-      title: "Mindfulness",
-    },
-    { title: "Peace of Mind" },
-    {
-      title: "Exercise",
-      description:''
-    },
-  ];
-
-  const btnstyle={
-    padding:'10px',
-    fontSize:"1.4rem",
-    marginRight:'10px'
-  
-    
-  }
-
   return (
-    <div >
-    <h1>This Home heathcare Page</h1>
-    <div className="btnmap">
-    {Healthlinks.map(function(links){
-        return <button style={btnstyle}>{links.title}</button>
-    })}
+    <div>
+      <h1>This Home heathcare Page</h1>
     </div>
-   </div>
   );
 };
 
--- fame_health_solution/src/HomeLinks/HomepageHeading.jsx
+++ fame_health_solution/src/HomeLinks/HomepageHeading.jsx
@@ -4,31 +4,67 @@ import { CgFileDocument } from "react-icons/cg";
 import { BsCardText, BsPeopleFill } from "react-icons/bs";
 
 const HomepageHeading = () => {
-const headingbg ={
-  backgroundImage:"url(/Images/headingbg.jpg)",
-  backgroundSize:'cover',
-  backgroundPosition:'center',
-  backgroundAttachment: 'fixed',
-  padding:'130px'
-}
+  const headingbg = {
+    backgroundImage: "url(/Images/headingbg.jpg)",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundAttachment: "fixed",
+    paddingBottom: "20%",
+  };
 
   return (
     <div className="homepageheading" style={headingbg}>
-      <div style={{ border: "2px solid gray" }}>
-        <CgFileDocument  style={{fontSize:'2rem'}} />
-        <p>About us</p>
+      <div>
+        <button className="headingbtnn" 
+          style={{
+            margin: "10px",
+            paddingLeft: "10px",
+            paddingRight: "10px",
+            borderRadius: "10px",
+            backgroundColor:'#ffff'
+          }}
+        >
+          <CgFileDocument style={{ fontSize: "2rem" }} />
+          <span style={{ display: "block" }}>About us</span>
+        </button>
+        <button className="headingbtnn"
+          style={{
+            margin: "10px",
+            paddingLeft: "10px",
+            paddingRight: "10px",
+            borderRadius: "10px",
+            backgroundColor:'#ffff'
+          }}
+        >
+          <AiOutlineWechat style={{ fontSize: "2rem" }} />
+          <span style={{ display: "block" }}>Chat with us</span>
+        </button>
       </div>
-      <div style={{ border: "2px solid gray" }}>
-        <AiOutlineWechat style={{fontSize:'2rem'}} />
-        <p>Chat with us</p>
-      </div>
-      <div style={{ border: "2px solid gray" }}>
-        <BsCardText style={{fontSize:'2rem'}}  />
-        <p>Mission Statment</p>
-      </div>
-      <div style={{ border: "2px solid gray" }}>
-        <BsPeopleFill style={{fontSize:'2rem'}}  />
-        <p>Founders</p>
+      <div className="headingbtn">
+        <button className="headingbtnn"
+          style={{
+            margin: "10px",
+            paddingLeft: "10px",
+            paddingRight: "10px",
+            borderRadius: "10px",
+            backgroundColor:'#ffff',
+          }}
+        >
+          <BsCardText style={{ fontSize: "2rem" }} />
+          <span style={{ display: "block" }}>Mission Statment</span>
+        </button>
+        <button className="headingbtnn"
+          style={{
+            margin: "10px",
+            paddingLeft: "10px",
+            paddingRight: "10px",
+            borderRadius: "10px",
+            backgroundColor:'#ffff'
+          }}
+        >
+          <BsPeopleFill style={{ fontSize: "2rem" }} />
+          <span style={{ display: "block" }}>Founders</span>
+        </button>
       </div>
     </div>
   );
